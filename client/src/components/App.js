96c1533093da11c221165a4163ad6e03bedf5a17
jasonchhay <jasonchhay@gmail.com>
2020-08-10 01:52:07 -0400

    Added material ui styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import Login from './components/Login';
+import Login from './Login';
 
 function App() {
 	return (
--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+
+import colors from '../utils/colors';
+
+const useStyles = makeStyles({
+	container: (props) => ({
+		backgroundColor: props.backgroundColor,
+		color: props.color,
+		padding: '.5rem 1.5rem',
+		borderRadius: '2rem',
+		margin: '1em',
+		boxShadow: '0 2px 2px rgba(0, 0, 0, 0.25)',
+		transition: '.2s ease-in',
+
+		'&:hover': {
+			color: colors.black,
+		},
+	}),
+});
 
 function AppButton({
 	title,
-	image = null,
-	color = '#fff',
-	backgroundColor = '#000',
+	image,
+	color = colors.white,
+	backgroundColor = colors.black,
 	...otherProps
 }) {
-	const style = {
-		container: {
-			backgroundColor: backgroundColor,
-			color: color,
-			padding: '.5rem 1.5rem',
-			borderRadius: '2rem',
-			margin: '1em',
-			boxShadow: '0 2px 2px rgba(0, 0, 0, 0.25)',
-		},
-	};
-
+	const classes = useStyles({ color, backgroundColor });
 	return (
-		<Button className='button' {...otherProps} style={style.container}>
+		<Button className={`${classes.container}`} {...otherProps}>
 			{image}
 			{title}
 		</Button>
