cc88fd2b65717f7da81da74c598c993dca4acfce
jasonchhay <jasonchhay@gmail.com>
2020-08-19 16:36:13 -0400

    Preparing for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +9,15 @@ app.use(cors()).use(cookieParser());
 
 // Define Routes
 app.use('/api', require('./routes'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 8888;
 
